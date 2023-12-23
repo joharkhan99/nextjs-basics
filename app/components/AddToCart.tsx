5a8cb57cb8431dcd85eb5419a100b86315d177aa
single-byte <joharkhan1999@gmail.com>
2023-12-24 00:39:06 +0500

    Update CSS and add link and product card components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+            <button
+      onClick={()=>{ console.log("object");}}
+      >
+        Click mes
+      </button>
+    </div>
+  )
+}
+
+export default AddToCart
